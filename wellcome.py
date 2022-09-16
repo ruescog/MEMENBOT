838bed9067cf81b263269c3879aec797d269770c
ruescog <ruescog@unirioja.es>
2022-09-16 22:36:20 +0200

    wellcome message multiple translates disabled

--- wellcome.py
+++ wellcome.py
@@ -5,11 +5,13 @@ from typing import Optional
 class Menu(discord.ui.View):
     def __init__(self, *, timeout: Optional[float] = 180):
         super().__init__(timeout = timeout)
-        self.value = None
+        self.used = False
 
     @discord.ui.button(label = "Translate", style = discord.ButtonStyle.green, emoji = '🇺🇸')
     async def english(self, interaction: discord.Interaction, button: discord.ui.Button):
         "Toggle to english the language of the message sent."
-        blob = TextBlob(interaction.message.content)
-        response = str(blob.translate(from_lang = "es", to = "en"))
-        await interaction.response.edit_message(content = response)
+        if not self.used:
+            blob = TextBlob(interaction.message.content)
+            response = str(blob.translate(from_lang = "es", to = "en"))
+            await interaction.response.edit_message(content = response)
+            self.used = True
